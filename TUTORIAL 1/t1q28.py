e7bb3ce41c57c9adde71c8d060432ba03327abda
AJAL RAJ R M <144008523+ajalrajrm@users.noreply.github.com>
2025-03-12 18:27:36 +0530

    Delete TUTORIAL 1/t1q28.py

--- TUTORIAL 1/t1q28.py
+++
@@ -1,4 +0,0 @@
-low, high = map(int, input("Enter the lower and upper limits: ").split())
-sum_of_odds = sum(num for num in range(low, high + 1) if num % 2 != 0)
-
-print(f"Sum of odd numbers in the range: {sum_of_odds}")
